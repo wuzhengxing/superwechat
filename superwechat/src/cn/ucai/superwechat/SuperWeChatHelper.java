94cd3b4b892060c96bdc48b5a21c1687261a7057
wuzhengxing <873094117@qq.com>
2017-02-16 10:42:14 +0800

    修改不显示联系人列表的bug

--- superwechat/src/cn/ucai/superwechat/SuperWeChatHelper.java
+++ superwechat/src/cn/ucai/superwechat/SuperWeChatHelper.java
@@ -1190,6 +1190,38 @@ public class SuperWeChatHelper {
 
         isSyncingContactsWithServer = true;
 
+        NetDao.loadContact(appContext, EMClient.getInstance().getCurrentUser(), new OnCompleteListener<String>() {
+            @Override
+            public void onSuccess(String str) {
+                if (str != null) {
+                    Result result = ResultUtils.getListResultFromJson(str, User.class);
+                    Log.e(TAG, "asyncFetchContactsFromServer---result:" + result);
+                    if (result != null && result.isRetMsg()) {
+                        List<User> list = (List<User>) result.getRetData();
+                        if (list != null && list.size() > 0) {
+                            Map<String, User> userMap = new HashMap<String, User>();
+                            for (User u : list) {
+                                EaseCommonUtils.setAppUserInitialLetter(u);
+                                userMap.put(u.getMUserName(), u);
+                            }
+                            // save the contact list to cache
+                            getAppContactList().clear();
+                            getAppContactList().putAll(userMap);
+                            // save the contact list to database
+                            UserDao dao = new UserDao(appContext);
+                            dao.saveAppContactList(list);
+                            broadcastManager.sendBroadcast(new Intent(Constant.ACTION_CONTACT_CHANAGED));
+                        }
+                    }
+                }
+            }
+
+            @Override
+            public void onError(String error) {
+
+            }
+        });
+
         new Thread() {
             @Override
             public void run() {
@@ -1203,36 +1235,7 @@ public class SuperWeChatHelper {
                         notifyContactsSyncListener(false);
                         return;
                     }
-                    NetDao.loadContact(appContext, EMClient.getInstance().getCurrentUser(), new OnCompleteListener<String>() {
-                        @Override
-                        public void onSuccess(String str) {
-                            if (str != null) {
-                                Result result = ResultUtils.getListResultFromJson(str, User.class);
-                                Log.e(TAG, "asyncFetchContactsFromServer---result:" + result);
-                                if (result != null && result.isRetMsg()) {
-                                    List<User> list = (List<User>) result.getRetData();
-                                    if (list != null && list.size() > 0) {
-                                        Map<String, User> userMap = new HashMap<String, User>();
-                                        for (User u : list) {
-                                            EaseCommonUtils.setAppUserInitialLetter(u);
-                                            userMap.put(username, u);
-                                        }
-                                        // save the contact list to cache
-                                        getAppContactList().clear();
-                                        getAppContactList().putAll(userMap);
-                                        // save the contact list to database
-                                        UserDao dao = new UserDao(appContext);
-                                        dao.saveAppContactList(list);
-                                    }
-                                }
-                            }
-                        }
 
-                        @Override
-                        public void onError(String error) {
-
-                        }
-                    });
                     Map<String, EaseUser> userlist = new HashMap<String, EaseUser>();
                     for (String username : usernames) {
                         EaseUser user = new EaseUser(username);
